955fecb636e555682c5e2e836d1c5cc0c0b8b8e2
aadamson <aadamson@stanford.edu>
2015-05-10 22:05:15 -0700

    changed run.sh

--- utils.cpp
+++ utils.cpp
@@ -31,24 +31,6 @@ istream& operator>>(istream& s, VectorXd& m) {
 }
 }
 
-MatrixXd softmax(const MatrixXd &x) {
-  RowVectorXd m = x.colwise().maxCoeff();
-  MatrixXd t = (x.rowwise() - m).array().exp();
-  return t.array().rowwise() / t.colwise().sum().array();
-}
-
-MatrixXd smaxentp(const MatrixXd &y, const MatrixXd &r) {
-  return y-r;
-}
-
-MatrixXd relu(const MatrixXd &x) {
-  return x.array().max(0);
-}
-
-MatrixXd relup(const MatrixXd &x) { 
-  return (x.array() > 0).cast<double>();
-}
-
 double str2double(const string& s) {
   istringstream i(s);
   double x;
